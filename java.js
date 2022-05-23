23a56face17e8094c0af7499c45456f256d85e23
Marsella/R <rozic.marsela@gmail.com>
2022-05-23 22:08:04 +0200

    new

---
+++ java.js
@@ -0,0 +1,10 @@
+let navIcon = document.getElementById("nav-icon");
+let navSidebar = document.getElementById("sidebar");
+let closeIcon = document.getElementById("close-icon");
+navIcon.addEventListener("click", function () {
+  navSidebar.classList.add("show-sidebar");
+});
+
+closeIcon.addEventListener("click", function () {
+  navSidebar.classList.remove("show-sidebar");
+});
